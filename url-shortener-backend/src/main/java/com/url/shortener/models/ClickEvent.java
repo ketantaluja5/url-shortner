a72fa673addac28ed0a1efd80b7388e6ba506be8
ketantaluja5 <ketantaluja5@gmail.com>
2026-04-11 17:34:47 +0530

    Added basic files

---
+++ url-shortener-backend/src/main/java/com/url/shortener/models/ClickEvent.java
@@ -0,0 +1,23 @@
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClickEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
+    private UrlMapping urlMapping;
+}
---
+++ url-shortener-backend/src/main/java/com/url/shortener/models/User.java
@@ -0,0 +1,22 @@
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String role = "ROLE_USER";
+
+}
